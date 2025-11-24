5c3f851d19472047295232af5f00e953463d7330
HalilSentuerk <halilsenturk40@gmail.com>
2025-11-24 10:29:37 +0100

    VehicleView update

--- src/main/java/bimobile/views/VehicleView.java
+++ src/main/java/bimobile/views/VehicleView.java
@@ -10,7 +10,9 @@ import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import jakarta.annotation.security.PermitAll;
 
 import java.util.List;
 
@@ -19,7 +21,11 @@ import java.util.List;
  * - Formular zum Erfassen/Ändern von Basisdaten
  * - Buttons für "Speichern" und einfache Statusänderungen
  */
-@Route("vehicles")
+@Route(value = "vehicles", layout = MainLayout.class)
+@PageTitle("Fahrzeugverwaltung")
+@PermitAll
+
+
 public class VehicleView extends VerticalLayout {
 
     private final VehicleService vehicleService;
