a483248d3d0b1d9c0c500c8ff9be4b94542ec91c
LeoK1110 <Leonard.koechling@hotmail.com>
2025-12-02 17:12:22 +0100

    Delete src/main/java/bimobile/src/main directory

--- src/main/java/bimobile/src/main/java/bimobile/model/PriceCategory.java
+++
@@ -1,5 +0,0 @@
-package bimobile.model;
-
-public enum PriceCategory {
-    A, B, C, D
-}
--- src/main/java/bimobile/src/main/java/bimobile/model/VehicleStatus.java
+++
@@ -1,9 +0,0 @@
-package bimobile.model;
-
-public enum VehicleStatus {
-    AVAILABLE,        // verfügbar
-    RENTED,           // verliehen
-    IN_MAINTENANCE,   // in Wartung / HU
-    SCRAPPED,         // ausgemustert (Endzustand)
-    SOLD              // verkauft
-}
