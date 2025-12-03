bd1243490b43887eff2bcde9ab843df6772f1431
Jannick Maurice Braun <116038944+JannickBraun@users.noreply.github.com>
2025-12-03 14:27:45 +0100

    Login system updated Password hashed

---
+++ src/main/java/bimobile/config/DataInitializer.java
@@ -0,0 +1,63 @@
+package bimobile.config;
+
+import bimobile.dao.UserRepository;
+import bimobile.model.Role;
+import bimobile.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class DataInitializer {
+
+    private static final Logger logger = LoggerFactory.getLogger(DataInitializer.class);
+
+    @Bean
+    CommandLineRunner initUsers(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        return args -> {
+            logger.info("=== Prüfe Benutzer-Initialisierung ===");
+            logger.info("Anzahl User in DB: {}", userRepository.count());
+
+            if (userRepository.count() == 0) {
+                logger.info("Keine Benutzer gefunden - erstelle Testbenutzer...");
+
+                User director = new User(
+                        "Max",
+                        "Mustermann",
+                        "director@bimobile.de",
+                        passwordEncoder.encode("director123"),
+                        Role.MANAGING_DIRECTOR
+                );
+                userRepository.save(director);
+                logger.info("Testbenutzer erstellt: director@bimobile.de / director123");
+
+                User manager = new User(
+                        "Erika",
+                        "Musterfrau",
+                        "manager@bimobile.de",
+                        passwordEncoder.encode("manager123"),
+                        Role.GENERAL_MANAGER
+                );
+                userRepository.save(manager);
+                logger.info("Testbenutzer erstellt: manager@bimobile.de / manager123");
+
+                User employee = new User(
+                        "Hans",
+                        "Müller",
+                        "employee@bimobile.de",
+                        passwordEncoder.encode("employee123"),
+                        Role.EMPLOYEE
+                );
+                userRepository.save(employee);
+                logger.info("Testbenutzer erstellt: employee@bimobile.de / employee123");
+
+                logger.info("=== Testbenutzer-Initialisierung abgeschlossen ===");
+            } else {
+                logger.info("Benutzer existieren bereits, überspringe Initialisierung.");
+            }
+        };
+    }
+}
--- src/main/java/bimobile/views/LoginView.java
+++ src/main/java/bimobile/views/LoginView.java
@@ -1,7 +1,9 @@
 package bimobile.views;
 
 import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.Paragraph;
 import com.vaadin.flow.component.login.LoginForm;
+import com.vaadin.flow.component.login.LoginI18n;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.BeforeEnterEvent;
 import com.vaadin.flow.router.BeforeEnterObserver;
@@ -22,10 +24,28 @@ public class LoginView extends VerticalLayout implements BeforeEnterObserver {
         setAlignItems(Alignment.CENTER);
         setJustifyContentMode(JustifyContentMode.CENTER);
 
-        login.setAction("login");
-        login.setForgotPasswordButtonVisible(true);
+        LoginI18n i18n = LoginI18n.createDefault();
+        LoginI18n.Form i18nForm = i18n.getForm();
+        i18nForm.setTitle("Anmelden");
+        i18nForm.setUsername("E-Mail");
+        i18nForm.setPassword("Passwort");
+        i18nForm.setSubmit("Anmelden");
+        i18n.setForm(i18nForm);
 
-        add(new H1("Autovermietung"), login);
+        LoginI18n.ErrorMessage i18nErrorMessage = i18n.getErrorMessage();
+        i18nErrorMessage.setTitle("Anmeldung fehlgeschlagen");
+        i18nErrorMessage.setMessage("E-Mail oder Passwort ist falsch. Bitte versuchen Sie es erneut.");
+        i18n.setErrorMessage(i18nErrorMessage);
+
+        login.setI18n(i18n);
+        login.setAction("login");
+        login.setForgotPasswordButtonVisible(false);
+
+        add(
+                new H1("BI-Mobile"),
+                new Paragraph("Autovermietung"),
+                login
+        );
     }
 
     @Override
