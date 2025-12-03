d5152a990456411c2519dc2ba91c9852b3610401
LeoK1110 <Leonard.koechling@hotmail.com>
2025-12-03 14:59:22 +0100

    Add files via upload

---
+++ src/main/java/bimobile/dao/InvoiceRepository.java
@@ -0,0 +1,10 @@
+package bimobile.dao;
+
+import bimobile.model.Invoice;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface InvoiceRepository extends JpaRepository <Invoice, Long> {
+}
