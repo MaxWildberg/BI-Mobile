e190b2e34680a172631a1ed855346994a0a0bb82
benberlin01 <ben.berlin@fh-bielefeld.de>
2025-12-05 16:48:27 +0100

    Employee fix

--- src/main/java/bimobile/views/EmployeeView.java
+++ src/main/java/bimobile/views/EmployeeView.java
@@ -16,12 +16,14 @@ import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
 
+import jakarta.annotation.security.PermitAll;
 import jakarta.annotation.security.RolesAllowed;
 
 
 import java.util.List;
 
 @Route(value = "employees", layout = MainLayout.class)
+@PermitAll
 public class EmployeeView extends VerticalLayout {
 
     private final EmployeeController controller;
