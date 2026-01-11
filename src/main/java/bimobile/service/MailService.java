b05e1ce4f053e964cd31cd5562e002e306d54606
Jannick Maurice Braun <116038944+JannickBraun@users.noreply.github.com>
2026-01-11 19:44:30 +0100

    Updated

--- src/main/java/bimobile/service/MailService.java
+++ src/main/java/bimobile/service/MailService.java
@@ -2,6 +2,7 @@ package bimobile.service;
 
 import bimobile.model.Invoice;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
@@ -21,6 +22,9 @@ import jakarta.mail.internet.MimeMessage;
 @Service
 public class MailService {
 
+    @Value("${spring.mail.username}")
+    private String mailForm;
+
     private final JavaMailSender mailSender;
 
     @Autowired
@@ -33,7 +37,7 @@ public class MailService {
             MimeMessage message = mailSender.createMimeMessage();
             MimeMessageHelper helper = new MimeMessageHelper(message, true);
 
-            helper.setFrom("leonard.koechling@hsbi.de");
+            helper.setFrom(mailForm);
             helper.setTo(to);
             helper.setSubject("Ihre BI-Mobile Rechnung");
             String htmlText =
